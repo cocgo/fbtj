9e686f320e27baa4b4f6e4e870f0eb506e6af51e
home <521401@qq.com>
2018-12-10 01:24:02 +0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require('path');
+var app = express();
+
+app.set('views', path.join(__dirname, 'views')); //设置模版路径在views目录（默认）
+app.set("view engine", 'ejs'); //模版引擎设置为 ejs
+
+app.get("/", function (req, res) {
+    // views目录下寻找'index.ejs'
+    // res.render(路径， 数据对象)
+    res.render('index.ejs', {
+        title: "演示",
+        message: "点击次数",
+        item: ['a', 'b', 'c']
+    })
+})
+
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
